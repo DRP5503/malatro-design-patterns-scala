bc2ae9ebb5bc2aff2e80b87f4881ea62fcbe47eb
Diego Ramirez Perez <diegoramirezp5503@gmail.com>
2026-03-22 14:29:32 -0300

    entrega-final-=

--- src/main/scala/mcd/euclides.scala
+++ src/main/scala/mcd/euclides.scala
@@ -11,14 +11,26 @@ y luego usarlo como deseen
 
   val res = a + 5
   println(s"El resultado del número ingresado más 5 es $res")
-  
+
 */
 import scala.io.StdIn.readInt
 
 @main def euclidesInteractivo(): Unit = {
   // Inicio de la zona donde deben editar el código
-  
-  println("No le sé al máximo común divisor :(")
-  
+  def calculo_mcd(a: Int, b: Int): Int =
+    if b == 0 then a
+    else calculo_mcd(b, a % b)
+
+  var seguir: Boolean = true
+  while seguir do
+    println("Ingrese el primer numero (escriba 0 para salir)")
+    val n1: Int = readInt()
+    if n1 == 0 then
+      seguir = false
+    else
+      println("Ingrese el segundo numero")
+      val n2: Int = readInt()
+      val res: Int = calculo_mcd(n1, n2)
+      println(s"El mcd entre  $n1  y  $n2  es  $res")
   // Fin de la zona donde deben editar el código
 }
