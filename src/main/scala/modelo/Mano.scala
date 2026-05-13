85ffdd9e50d260a2d2564268cb8ac4a98d455b12
Diego Ramirez Perez <diegoramirezp5503@gmail.com>
2026-05-12 22:16:47 -0400

    feat: Implementaion de metodos para Mano
    Se implementaron 5 nuevos metodos para Mano, enfocados en poder manipular las cartas de una mano y jugar las cartas seleccionadas

--- src/main/scala/modelo/Mano.scala
+++ src/main/scala/modelo/Mano.scala
@@ -3,4 +3,34 @@ package modelo
 
 import modelo.cartas.{Carta, Joker}
 
-class Mano(val cartas: List[Carta], val jokers: List[Joker])
+class Mano(var cartas: List[Carta], var jokers: List[Joker]) {
+  def añadirCartas(carta:Carta): Unit={
+    cartas = cartas :+ carta
+  }
+  def añadirJoker(joker: Joker): Unit={
+    jokers = jokers :+ joker
+  }
+  def eliminarCartas(indices: List[Int]): Unit={
+    var cartasNuevas = List[Carta]()
+    for (i <- cartas.indices) {
+      if (!indices.contains(i))
+        cartasNuevas = cartasNuevas :+ cartas(i)
+    }
+    cartas = cartasNuevas
+  }
+
+  def eliminarJoker(indices: List[Int]): Unit = {
+    var jokersNuevos = List[Joker]()
+    for (i <- jokers.indices) {
+      if (!indices.contains(i))
+        jokersNuevos = jokersNuevos :+ jokers(i)
+    }
+    jokers = jokersNuevos
+  }
+
+  def jugarCartas(indices: List[Int]): List[Carta] = {
+    val cartasJugadas = indices.map(i => cartas(i))
+    eliminarCartas(indices)
+    cartasJugadas
+  }
+}
