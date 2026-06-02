a052aed338607eedf788a30d06a4b6304d55c78b
Diego Ramirez Perez <diegoramirezp5503@gmail.com>
2026-06-01 21:33:21 -0400

    feat: Implementacion de los exception y nuevo metodo descartarCartas
    Implemente los exception definidos anteriormente en sus metodos correspondientes (metodos de la clase mano).
    Tambien fue creado el metodo necesario para descartarCartas. funcionamiento muy parecido a jugarCartas

--- src/main/scala/modelo/Mano.scala
+++ src/main/scala/modelo/Mano.scala
@@ -6,15 +6,24 @@ import modelo.cartas.{Carta, Joker}
 class Mano(private var _cartas: List[Carta], private var _jokers: List[Joker]) {
   def cartas: List[Carta] = _cartas
   def jokers : List[Joker] = _jokers
+  private var _descartes: Int = 0
+  def descartes: Int = _descartes
   
   def añadirCartas(carta:Carta): Unit={
-
+    if (_cartas.length == 8)
+      throw new ManoLlenaException("Mano llena")
     _cartas = _cartas :+ carta
   }
   def añadirJoker(joker: Joker): Unit={
+    if (_jokers.length == 2)
+      throw new JokersLlenosException("La mano ya tiene 2 Jokers")
     _jokers = _jokers :+ joker
   }
   def eliminarCartas(indices: List[Int]): Unit={
+    for (i <- indices) {
+      if (i < 0 || i >= _cartas.length)
+        throw  new IndiceCartaInvalidoException(s"El indice $i no es valido")
+    }
     var cartasNuevas = List[Carta]()
     for (i <- cartas.indices) {
       if (!indices.contains(i))
@@ -24,6 +33,10 @@ class Mano(private var _cartas: List[Carta], private var _jokers: List[Joker]) {
   }
 
   def eliminarJoker(indices: List[Int]): Unit = {
+    for (i <- indices) {
+      if (i < 0 || i >= _jokers.length)
+        throw new IndiceJokerInvalidoException(s"El indice $i no es valido")
+    }
     var jokersNuevos = List[Joker]()
     for (i <- jokers.indices) {
       if (!indices.contains(i))
@@ -33,8 +46,31 @@ class Mano(private var _cartas: List[Carta], private var _jokers: List[Joker]) {
   }
 
   def jugarCartas(indices: List[Int]): List[Carta] = {
+    if (indices.length > 5)
+      throw new CartasMaximasException("No se pueden jugar mas de 5 cartas")
+    if (indices.length < 1)
+      throw new CartasMinimasException("No se puede jugar menos de 1 carta")
+    for (i <- indices) {
+      if (i < 0 || i >= _cartas.length)
+        throw new IndicesCartasInvalidosException(s"El indice $i no es valido")
+    }
     val cartasJugadas = indices.map(i => cartas(i))
     eliminarCartas(indices)
     cartasJugadas
+  }
+
+  def descartarCartas(indices: List[Int]): Unit = {
+    if (_descartes == 3)
+      throw new LimiteDescartesException("No se puede descartar mas de 3 veces")
+    if (indices.length > 5)
+      throw new DescartesMaximosException("No se pueden descartar mas de 5 cartas")
+    if (indices.length < 1)
+      throw new DescartesMinimosException("No se puede descartar menos de 1 carta")
+    for (i <- indices) {
+      if (i < 0 || i >= _cartas.length)
+        throw new IndicesDescartesInvalidosException(s"El indice $i no es valido")
+    }
+    _descartes += 1
+    eliminarCartas(indices)
   }
 }
